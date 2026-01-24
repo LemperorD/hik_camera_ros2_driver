946fe54be9357950fc40c0ca80ce3f305a19fcae
LD <ld2382619813@163.com>
2026-01-24 11:34:47 +0800

    feat(hikROS): :heavy_plus_sign: 增加MVS bin目录进入hikROS

---
+++ hikSDK/bin/EventTool.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+ROOT_PATH=$(cd "$(dirname "$0")";pwd)
+
+export LD_LIBRARY_PATH=${ROOT_PATH}:~/nav2_ws/src/hik_camera_ros2_driver/hikSDK/lib/amd64
+
+if [ "$EUID" -ne 0 ]; then
+  echo "not root user" 
+else
+  echo "Run as root"
+  chmod -R 777 ${ROOT_PATH}/Cfg
+  chmod -R 777 ${ROOT_PATH}/Temp
+fi
+
+exec ${ROOT_PATH}/EventTool -platform xcb
